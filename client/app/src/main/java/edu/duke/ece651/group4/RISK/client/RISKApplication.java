402b33ebd1e953e12fb4a548b501f0877bab7575
zian <gengz@lafayette.edu>
2021-03-31 13:12:41 -0400

    application

--- client/app/src/main/java/edu/duke/ece651/group4/RISK/client/RISKApplication.java
+++ client/app/src/main/java/edu/duke/ece651/group4/RISK/client/RISKApplication.java
@@ -1,4 +1,5 @@
 package edu.duke.ece651.group4.RISK.client;
+import java.util.Random;
 
 import android.app.Application;
 import android.content.SharedPreferences;
@@ -6,12 +7,18 @@ import android.content.SharedPreferences;
 import java.net.Socket;
 
 public class RISKApplication extends Application {
-    private Socket mySocket;
+    private Client playerClient;
+    private World theWorld;
+    private final int totalPopulation;
+    private Random rnd;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mySocket = null;
+        this.playerClient=new Client("localhost", "9999");
+        this.theWorld=null;
+        this.totalPopulation=15;
+        this.rnd=new Random();
     }
 
 }
