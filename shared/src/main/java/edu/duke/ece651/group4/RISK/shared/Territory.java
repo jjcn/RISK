a0418d3e559fb9d11aa2c6f91c050481adf3cb60
Shengxi Jin <sj320@duke.edu>
2021-04-03 14:20:38 -0400

    formatted Territory, Troop

--- shared/src/main/java/edu/duke/ece651/group4/RISK/shared/Territory.java
+++ shared/src/main/java/edu/duke/ece651/group4/RISK/shared/Territory.java
@@ -14,25 +14,23 @@ public class Territory implements Serializable {
 
     private Troop ownerTroop;
 
-    private final HashMap<String,Troop> enemyOnTerritory;
+    private final HashMap<String, Troop> enemyOnTerritory;
 
     private Random rnd;
 
     public Territory(String name, int size, 
                     Troop ownerTroop, 
-                    HashMap<String,Troop> enemyOnTerritory,
+                    HashMap<String, Troop> enemyOnTerritory, 
                     Random rnd) {
-            this.name = name;
-            this.size = size;
-            this.enemyOnTerritory = enemyOnTerritory;
-            this.ownerTroop = ownerTroop;
-            this.rnd = rnd;
-        }
-    
-
+        this.name = name;
+        this.size = size;
+        this.enemyOnTerritory = enemyOnTerritory;
+        this.ownerTroop = ownerTroop;
+        this.rnd = rnd;
+    }
 
     public Territory(String name, 
-                    Player owner, int population,
+                    Player owner, int population, 
                     Random rnd) {
         this.name = name;
         this.size = 0;
@@ -43,7 +41,7 @@ public class Territory implements Serializable {
 
     public Territory(String name, 
                     Troop ownerTroop, 
-                    HashMap<String,Troop> enemyOnTerritory,
+                    HashMap<String, Troop> enemyOnTerritory, 
                     Random rnd) {
         this.name = name;
         this.size = 0;
@@ -78,6 +76,7 @@ public class Territory implements Serializable {
 
     /**
      * Send out specific number of unit from territory
+     * 
      * @param subTroop shows the number of unit send out from territory
      */
     public Troop sendOutTroop(Troop subTroop) {
@@ -86,6 +85,7 @@ public class Territory implements Serializable {
 
     /**
      * Send in specific number of unit to territory
+     * 
      * @param subTroop shows the number of unit send in to territory
      */
     public void sendInTroop(Troop subTroop) {
@@ -94,88 +94,90 @@ public class Territory implements Serializable {
 
     /**
      * Send in specific number of enemy to territory
+     * 
      * @param enemy shows the enemy troop attack in
      */
     public void sendInEnemyTroop(Troop enemy) {
-        this.enemyOnTerritory.put(enemy.getOwner().getName(),enemy);
+        this.enemyOnTerritory.put(enemy.getOwner().getName(), enemy);
     }
 
     /**
      * Do battle between two troops
+     * 
      * @param enemy shows the enemy troop attack in
      */
-    public void doOneBattle(Troop enemy){
-        Troop enemyRemain=this.ownerTroop.combat(enemy);
-        this.ownerTroop = this.ownerTroop.checkWin()?this.ownerTroop:enemyRemain;
+    public void doOneBattle(Troop enemy) {
+        Troop enemyRemain = this.ownerTroop.combat(enemy);
+        this.ownerTroop = this.ownerTroop.checkWin() ? this.ownerTroop : enemyRemain;
     }
-
 
     public Player getOwner() {
         return this.ownerTroop.getOwner();
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
     public int getSize() {
         return this.size;
     }
-    
+
     /**
      * Added specific number of unit to territory
+     * 
      * @param num shows number added
      */
-    public void addUnit(int num){
-        Troop newTroop=new Troop(num,this.getOwner());
+    public void addUnit(int num) {
+        Troop newTroop = new Troop(num, this.getOwner());
         this.sendInTroop(newTroop);
     }
 
     /**
      * Initialize population and owner of territory
-     * @param num shows number added
-     *  @param owner shows owner of territory
+     * 
+     * @param num   shows number added
+     * @param owner shows owner of territory
      */
-    public void initializeTerritory(int num, Player owner){
-        this.ownerTroop=new Troop(num,owner);
+    public void initializeTerritory(int num, Player owner) {
+        this.ownerTroop = new Troop(num, owner);
     }
 
     /**
      * Check population of territory
      */
-    public int checkPopulation(){
+    public int checkPopulation() {
         return this.ownerTroop.checkTroopSize();
     }
-
 
     /**
      * Do all the battles with enemies the sequence is random
      */
-    public String doBattles(){
+    public String doBattles() {
 
-        StringBuilder report=new StringBuilder();
+        StringBuilder report = new StringBuilder();
 
-        if(this.enemyOnTerritory.size()==0){
-            return "no war on Territory "+this.getName()+"\n";
+        if (this.enemyOnTerritory.size() == 0) {
+            return "no war on Territory " + this.getName() + "\n";
         }
 
         ArrayList<String> enemyPlayers = new ArrayList<String>(this.enemyOnTerritory.keySet());
         Collections.sort(enemyPlayers);
-        while(enemyPlayers.size()>0){
+        while (enemyPlayers.size() > 0) {
 
-            int diceResult=randInt(0,enemyPlayers.size()-1);
+            int diceResult = randInt(0, enemyPlayers.size() - 1);
 
-            String enemy=enemyPlayers.get(diceResult);
-            report.append("Enemy "+enemy+" fight with "+this.getOwner().getName()+" on "+this.getName()+" \n");
-            Troop enemyTroop=this.enemyOnTerritory.get(enemy);
+            String enemy = enemyPlayers.get(diceResult);
+            report.append(
+                    "Enemy " + enemy + " fight with " + this.getOwner().getName() + " on " + this.getName() + " \n");
+            Troop enemyTroop = this.enemyOnTerritory.get(enemy);
             doOneBattle(enemyTroop);
             enemyPlayers.remove(diceResult);
-            report.append(this.getOwner().getName()+" wins the fight and owns "+this.getName()+" \n");
+            report.append(this.getOwner().getName() + " wins the fight and owns " + this.getName() + " \n");
         }
         this.enemyOnTerritory.clear();
         return report.toString();
     }
-
 
     private int randInt(int min, int max) {
         int randomNum = rnd.nextInt((max - min) + 1) + min;
@@ -186,46 +188,36 @@ public class Territory implements Serializable {
     @Override
     public boolean equals(Object other) {
         if (other != null && other.getClass().equals(getClass())) {
-            Territory otherTerritory = (Territory)other;
+            Territory otherTerritory = (Territory) other;
             return name.equals(otherTerritory.name);
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public void setRandom(Random seed){
-        this.rnd=seed;
+    public void setRandom(Random seed) {
+        this.rnd = seed;
     }
 
-    public Territory clone(){
-        HashMap<String,Troop> cpy=new HashMap<>();
-        for(String s:this.enemyOnTerritory.keySet()){
-            cpy.put(new String(s),this.enemyOnTerritory.get(s).clone());
+    public Territory clone() {
+        HashMap<String, Troop> cpy = new HashMap<>();
+        for (String s : this.enemyOnTerritory.keySet()) {
+            cpy.put(new String(s), this.enemyOnTerritory.get(s).clone());
         }
-        Territory clone= new Territory(new String(this.name),ownerTroop.clone(),cpy,this.rnd);
+        Territory clone = new Territory(new String(this.name), ownerTroop.clone(), cpy, this.rnd);
         return clone;
     }
 
-
     /**
      * @author Shengxi Jin
-     * @param before is the level of unit before upgrade.
-     * @param after is the level of unit after the upgrade.
-     * @param nUnit is the number of units to upgrade.
+     * @param before    is the level of unit before upgrade.
+     * @param after     is the level of unit after the upgrade.
+     * @param nUnit     is the number of units to upgrade.
      * @param nResource is the quantity of resource at hand.
      * @return remaining resource after the upgrade.
      */
-    public int upgrade(int before, int after, int nUnit, 
-                       int nResource) {
+    public int upgrade(int before, int after, int nUnit, int nResource) {
         String jobName = String.format("Soldier LV%d", before); // TODO: this is hardcoded
         return ownerTroop.updateUnit(jobName, after - before, nUnit, nResource);
     }
 }
-
-
-
-
-
-
-
