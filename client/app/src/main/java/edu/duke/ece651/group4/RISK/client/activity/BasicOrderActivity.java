0575d619ecf2eecf6a38b1cce68389a1ac243500
WenxinXu <wx65@duke.edu>
2021-04-06 09:59:43 +0800

    imp BasicOrderActivity

--- client/app/src/main/java/edu/duke/ece651/group4/RISK/client/activity/BasicOrderActivity.java
+++ client/app/src/main/java/edu/duke/ece651/group4/RISK/client/activity/BasicOrderActivity.java
@@ -1,8 +1,15 @@
 package edu.duke.ece651.group4.RISK.client.activity;
 
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.duke.ece651.group4.RISK.client.R;
+import edu.duke.ece651.group4.RISK.shared.Territory;
+
+import java.util.List;
+
+import static edu.duke.ece651.group4.RISK.client.RISKApplication.*;
 
 public class BasicOrderActivity extends AppCompatActivity {
 
@@ -10,5 +17,26 @@ public class BasicOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_order);
+        impUI();
+    }
+
+    private void impUI() {
+        List<Territory> terrChoices = getMyTerritory();
+
+//        terrsAdapter = new ArrayAdapter<>(TurnActivity.this, R.layout.item_choice, worldInfo);
+//        worldInfoRC.setAdapter(worldInfoAdapter);
+//
+//        List<String> worldInfo = getWorldInfo();
+//        worldInfoAdapter = new ArrayAdapter<>(TurnActivity.this, R.layout.item_choice, worldInfo);
+//        worldInfoRC.setAdapter(worldInfoAdapter);
+//
+//        List<String> worldInfo = getWorldInfo();
+//        worldInfoAdapter = new ArrayAdapter<>(TurnActivity.this, R.layout.item_choice, worldInfo);
+//        worldInfoRC.setAdapter(worldInfoAdapter);
+
+        Button commitBT = findViewById(R.id.commit_button);
+        commitBT.setOnClickListener(v->{
+           // String result = doOneMove(buildMoveOrder(src,des,num,job));
+        });
     }
 }
