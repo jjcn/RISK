8e22fc182f2c06e393689c68c63be92f6bc8fcfa
unknown <sj320@duke.edu>
2021-04-20 13:21:08 -0400

    add transfer troop order

--- shared/src/main/java/edu/duke/ece651/group4/RISK/shared/TransferTroopOrder.java
+++ shared/src/main/java/edu/duke/ece651/group4/RISK/shared/TransferTroopOrder.java
@@ -5,23 +5,49 @@ import java.util.NoSuchElementException;
 
 /**
  * An order that transfers troop on a territory to another type.
- * @param srcName is the name of territory to upgrade its troop.
- * @param levelBefore is the level of unit before upgrade.
- * @param levelAfter is the level of unit after the upgrade.
- * @param nUnit is the number of units to upgrade.
+ * Does not change unit level.
+ *
+ * Usage demo:
+ *
+ * In order to transfer two LV3 soldier -> LV3 knight on a territory named "Hogwarts",
+ *
+ * construct a TransferTroopOrder as follows:
+ *
+ * TransferTroopOrder ttOrder = new TransferTroopOrder("Hogwarts", Constant.SOLDIER, Constant.KNIGHT, 3, 2);
+ *
  */
 public class TransferTroopOrder implements Order, Serializable {
-
+    /**
+     * Error messages
+     */
     protected static final String NO_DES_MSG =
             "Place order does not have destination territory.";
     protected static final String NO_TROOP_MSG =
             "Place order does not have troop.";
 
+    /**
+     * A character that uniquely identifies what type of action it is.
+     */
     private Character actionName;
+    /**
+     * Name of the territory on which troop will be upgraded.
+     */
     private String srcName;
+    /**
+     * Type of unit before upgrade.
+     */
     private String typeBefore;
+    /**
+     * Type of unit after upgrade.
+     */
     private String typeAfter;
+    /**
+     * Specify the level of unit to transfer.
+     */
     private int unitLevel;
+    /**
+     * Number of units to transfer.
+     */
     private int nUnit;
 
     public TransferTroopOrder(String srcName,
