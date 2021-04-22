19d614c20effd019b0b310b08fd9c0bc00115a72
unknown <sj320@duke.edu>
2021-04-22 16:30:04 -0400

    TODO: get ranged units range

--- shared/src/main/java/edu/duke/ece651/group4/RISK/shared/AttackOrderChecker.java
+++ shared/src/main/java/edu/duke/ece651/group4/RISK/shared/AttackOrderChecker.java
@@ -59,7 +59,7 @@ public class AttackOrderChecker implements Serializable {
             }
             // ranged units can only attack territories within range
             else {
-                if (world.calculateShortestPath(start, end) > troop) {
+                if (world.calculateShortestPath(start, end) > troop.getAttackRange()) {
                     return String.format(RANGED_OUT_OF_REACH_MSG,
                             end.getName(),
                             start.getName());
