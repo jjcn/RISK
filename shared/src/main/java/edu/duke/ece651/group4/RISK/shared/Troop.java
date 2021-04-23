ad8ea363cafa7b434edcfeae2cf3d28f556b1d64
zian <gengz@lafayette.edu>
2021-04-23 01:19:56 -0400

    bugfix

--- shared/src/main/java/edu/duke/ece651/group4/RISK/shared/Troop.java
+++ shared/src/main/java/edu/duke/ece651/group4/RISK/shared/Troop.java
@@ -73,7 +73,6 @@ public class Troop implements Serializable {
 
             for (int i = 0; i < myDict.get(s); i++) {
                 Soldier sol = new Soldier();
-                sol.setJob(s);
                 this.population.add(sol);
             }
         }
--- shared/src/test/java/edu/duke/ece651/group4/RISK/shared/TroopTest.java
+++ shared/src/test/java/edu/duke/ece651/group4/RISK/shared/TroopTest.java
@@ -237,11 +237,14 @@ public class TroopTest {
     }
 
     @Test
-    public void Test_clone() {
+    public void Test_clone_bug() {
+        HashMap<String, Integer> dict = new HashMap<>();
+        dict.put(BREAKER_NAMES.get(0), 4);
+        Troop troop1= new Troop(dict, new TextPlayer("test1"));
 //        Troop troop1 = new Troop(10, new TextPlayer("test1"), new Random(0));
 //        troop1.transfer(SOLDIER,BREAKER, 0,2);
-//        Troop test=troop1.clone();
-//        System.out.println(test.getSummary());
+        Troop test=troop1.clone();
+        System.out.println(test.getSummary());
 
     }
 
